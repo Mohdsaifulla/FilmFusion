7a2623b4be35ab54ebb205c90e347d2c5be5ff6a
Mohdsaifulla <saif49032@gmail.com>
2024-03-28 14:47:15 +0530

    Login and singUp page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <div className="absolute px-16 py-4 bg-gradient-to-b from-black w-full">
+        <img src="/Netflix_logo.png" alt="logo" className="w-40" />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSingIn, setIsSignIn] = useState(true);
+  const toggleSignUp = () => {
+    setIsSignIn(!isSingIn);
+  };
   return (
-    <div>
-      <Header />
+    <div className="h-screen bgImage flex flex-col gap-20 relative">
+      <div className="h-[15%]">
+        <Header />
+      </div>
+      <form className="bg-black opacity-80 flex flex-col sm:w-[400px]  gap-4 rounded-lg p-4 absolute md:top-[15%] w-full md:left-[32%]  top-40">
+        <label className="text-3xl font-semibold text-white pt-6 mx-4">
+          {isSingIn ? "Sing in" : "Sign Up"}
+        </label>
+        {!isSingIn && (
+          <input
+            type="text"
+            placeholder="Name"
+            className="bg-transparent border p-4 mx-4 rounded"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email"
+          className="bg-transparent border p-4 mx-4 rounded"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="bg-transparent border p-4 mx-4 rounded"
+        />
+        <button className="bg-red-600 rounded text-xl font-semibold p-2 mx-4 text-white">
+          {isSingIn ? "Sing in" : "Sign Up"}
+        </button>
+        <div className="flex gap-2 text-white p-2 mx-4">
+          <p className="">New to Netflix?</p>
+          <p className="font-semibold cursor-pointer" onClick={toggleSignUp}>
+            Sign up Now
+          </p>
+        </div>
+      </form>
+
+      <div className=" absolute bottom-0 flex flex-col bg-black opacity-80 text-gray-400 w-full gap-4 px-16 py-8">
+        <div>
+          <p>Questions? Call 000-800-919-1694</p>
+        </div>
+        <div className="flex gap-4">
+          <p>FAQ</p> <p>QHelp Center</p> <p>Terms of Use</p>
+        </div>
+        <div className="flex gap-4">
+          <p>Privacy</p>
+          <p>Coprporate Information</p>
+          <p>Cookie Preferences</p>
+        </div>
+      </div>
     </div>
   );
 };
